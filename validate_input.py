9fc3c5806a41be8e3ca94a3104019c9e93100100
marcus-elia <54640981+marcus-elia@users.noreply.github.com>
2020-01-31 16:18:12 -0500

    Replaced if/else with list checks

--- validate_input.py
+++ validate_input.py
@@ -12,22 +12,14 @@ if len(words) < 3:
     print("Invalid Input: Not enough query terms.")
 
 if words[1] == "professor":
-    if words[0] == "office":
-        # do things
-    elif words[0] == "position":
-        # do things
+    if words[0] in valid_professor_words:
+        # try doing the query
     else:
         print("Invalid Input: First query term must be either \"office\" or \"position\"")
 
 elif words[1] == "courses":
-    if words[0] == "crn":
-        # do things
-    elif words[0] == "enrollment":
-        # do things
-    elif words[0] == "classroom":
-        # do things
-    elif words[0] == "professor":
-        # do things
+    if words[0] in valid_course_words:
+        # try doing the query
     else:
         print("Invalid Input: First query term must be either \"crn\" or \"enrollment\" or \"classroom\" or \"professor\"")
 else:
