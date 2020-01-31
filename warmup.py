92edb1482a6f72f44bf807d023f93ecbd6b6fefe
brythibault1 <44148781+brythibault1@users.noreply.github.com>
2020-01-31 16:17:44 -0500

    Update warmup.py
    Adding more code to actually create the tables

--- warmup.py
+++ warmup.py
@@ -12,9 +12,35 @@ classSize = 'Number of Students'
 professor = 'Professor'
 office = 'Office Location'
 columnType = 'TEXT'
+courseTable = """
+  CREATE TABLE courses (
+       id integer PRIMARY KEY,
+       crn integer NOT NULL,
+       classSize integer NOT NULL,
+       professor text NOT NULL )
+       """
+professorTable = """
+   CREATE TABLE professors (
+        id integer PRIMARY KEY,
+        professor text NOT NULL,
+        office text NOT NULL )
+        """
+# Creating the connection to the two tables and database
+try:
+    sqliteConnection1 = sqlite3.connect(sqliteFile1)
+    sqliteConnection2 = sqlite3.connect(sqliteFile2)
+    cursor = sqliteConnection.cursor()
+    print("Database created successfully and connected successfully to SQLite")
 
-cur.execute("CREATE TABLE t1 (id, courses, crn, classSize, professor);")
-cur.execute("CREATE TABLE t2 (id, professor, office);")
+    c.execute(courseTable);
+    c.execute(professorTable);
+    
+except sqlite3.Error as error:
+    print("Error connecting to database",error)
+finally:
+    if(sqliteConnection):
+        sqliteConnection.close()
+        print("The connection is closed")
 
 with open('courses.csv','rb') as fin:
     dr = csv.DictReader(fin)
